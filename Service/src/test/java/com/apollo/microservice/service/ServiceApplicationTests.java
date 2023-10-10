7747a357591301720e61774428a53bac9b5e305e
Vinnih-1 <vinifantal@hotmail.com>
2023-10-10 15:19:09 -0300

    feat: add new microservice

---
+++ Service/src/test/java/com/apollo/microservice/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apollo.microservice.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
