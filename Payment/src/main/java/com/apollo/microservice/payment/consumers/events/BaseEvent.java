d4758aa1d7f6b1238470e0c55943a1f296c925ce
Vinnih-1 <vinifantal@hotmail.com>
2023-10-18 22:01:02 -0300

    add: base to all payment events

---
+++ Payment/src/main/java/com/apollo/microservice/payment/consumers/events/BaseEvent.java
@@ -0,0 +1,14 @@
+package com.apollo.microservice.payment.consumers.events;
+
+import com.apollo.microservice.payment.dto.PaymentDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public abstract class BaseEvent {
+
+    private final String event;
+
+    public abstract void execute(PaymentDTO paymentDTO);
+}
---
+++ Service/src/main/java/com/apollo/microservice/service/consumers/events/BaseEvent.java
@@ -0,0 +1,14 @@
+package com.apollo.microservice.service.consumers.events;
+
+import com.apollo.microservice.service.models.PaymentModel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public abstract class BaseEvent {
+    
+    private final String event;
+    
+    public abstract void execute(PaymentModel paymentModel);
+}
