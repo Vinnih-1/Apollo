6fd58eaf9ec6a1ed53e3f6b5f74b1cbcfc27005b
Vinnih-1 <vinifantal@hotmail.com>
2023-10-18 22:06:43 -0300

    add: find products by service id method

--- Service/src/main/java/com/apollo/microservice/service/repositories/ProductRepository.java
+++ Service/src/main/java/com/apollo/microservice/service/repositories/ProductRepository.java
@@ -3,5 +3,10 @@ package com.apollo.microservice.service.repositories;
 import com.apollo.microservice.service.models.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<ProductModel, Long> {
+
+    Optional<List<ProductModel>> findProductsByServiceId(String serviceId);
 }
