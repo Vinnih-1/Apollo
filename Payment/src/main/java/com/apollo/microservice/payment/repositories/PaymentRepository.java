4851663379ede56f3ff60ae3442bf448a83e7f73
Vinnih-1 <vinifantal@hotmail.com>
2023-10-19 16:59:08 -0300

    add: find expired payments query

--- Payment/src/main/java/com/apollo/microservice/payment/repositories/PaymentRepository.java
+++ Payment/src/main/java/com/apollo/microservice/payment/repositories/PaymentRepository.java
@@ -2,10 +2,16 @@ package com.apollo.microservice.payment.repositories;
 
 import com.apollo.microservice.payment.models.PaymentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Calendar;
+import java.util.List;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<PaymentModel, String> {
 
     Optional<PaymentModel> findByPayer(String payer);
+
+    @Query("SELECT p FROM PaymentModel p WHERE p.expirateAt < ?1")
+    List<PaymentModel> findExpiredPayments(Calendar currentDate);
 }
