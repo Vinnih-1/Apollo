07f5b2dc5acd3f087dd414ba8c10ceee4cc12b8c
Vinnih-1 <vinifantal@hotmail.com>
2023-10-20 16:22:17 -0300

    fix: debug

--- Service/src/main/java/com/apollo/microservice/service/consumers/events/PaymentSuccessEvent.java
+++ Service/src/main/java/com/apollo/microservice/service/consumers/events/PaymentSuccessEvent.java
@@ -14,6 +14,6 @@ public class PaymentSuccessEvent extends BaseEvent {
 
     @Override
     public void execute(PaymentModel paymentModel) {
-        System.out.println("pagamento aprovado");
+        System.out.println("Pagando Aprovado: " + paymentModel.toString());
     }
 }
