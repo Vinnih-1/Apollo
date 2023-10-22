687cdac189965df65a19e461cf3a758ace2e01ce
Vinnih-1 <vinifantal@hotmail.com>
2023-10-21 21:10:36 -0300

    add: create producer and consumer dto

---
+++ Discord/src/main/java/com/microservice/discord/dtos/AuthorizeConsumerDTO.java
@@ -0,0 +1,4 @@
+package com.microservice.discord.dtos;
+
+public record AuthorizeConsumerDTO(String authorizeUrl) {
+}
---
+++ Discord/src/main/java/com/microservice/discord/dtos/AuthorizeProducerDTO.java
@@ -0,0 +1,4 @@
+package com.microservice.discord.dtos;
+
+public record AuthorizeProducerDTO(String serviceKey, String discordId) {
+}
