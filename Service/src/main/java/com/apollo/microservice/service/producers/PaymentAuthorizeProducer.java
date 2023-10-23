ea6d70e672ffc23ec0fb384842e7e991111d72e3
Vinnih-1 <vinifantal@hotmail.com>
2023-10-23 00:42:41 -0300

    add: payment authorizer producer

---
+++ Service/src/main/java/com/apollo/microservice/service/producers/PaymentAuthorizeProducer.java
@@ -0,0 +1,28 @@
+package com.apollo.microservice.service.producers;
+
+import com.apollo.microservice.service.models.AuthorizeModel;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentAuthorizeProducer {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Value(value = "${broker.queue.auth.payment}")
+    private String paymentRoutingKey;
+
+    @Value(value = "${broker.queue.auth.service}")
+    private String discordRoutingKey;
+
+    public void publishPaymentAuthorizeRequest(AuthorizeModel authorizeModel) {
+        rabbitTemplate.convertAndSend("", paymentRoutingKey, authorizeModel);
+    }
+
+    public void publishPaymentAuthorizeResponse(AuthorizeModel authorizeModel) {
+        rabbitTemplate.convertAndSend("", discordRoutingKey, authorizeModel);
+    }
+}
