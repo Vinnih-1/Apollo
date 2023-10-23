16fdaa6b1a2aff5bb10069fe7ca3f9e499d7b837
Vinnih-1 <vinifantal@hotmail.com>
2023-10-23 00:43:23 -0300

    fix: unused imports

--- Service/src/main/java/com/apollo/microservice/service/configs/RabbitMQConfig.java
+++ Service/src/main/java/com/apollo/microservice/service/configs/RabbitMQConfig.java
@@ -1,8 +1,6 @@
 package com.apollo.microservice.service.configs;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
