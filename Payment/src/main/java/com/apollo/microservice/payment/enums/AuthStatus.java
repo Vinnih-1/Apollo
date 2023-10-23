601db5e479e5e0db3c0f3dc5fd2f9795b04487e6
Vinnih-1 <vinifantal@hotmail.com>
2023-10-23 00:40:45 -0300

    add: auth status

---
+++ Payment/src/main/java/com/apollo/microservice/payment/enums/AuthStatus.java
@@ -0,0 +1,6 @@
+package com.apollo.microservice.payment.enums;
+
+public enum AuthStatus {
+
+    LINK_PENDING, AUTH_PENDING, AUTH_SUCCESS, AUTH_EXPIRED
+}
---
+++ Service/src/main/java/com/apollo/microservice/service/enums/AuthStatus.java
@@ -0,0 +1,6 @@
+package com.apollo.microservice.service.enums;
+
+public enum AuthStatus {
+
+    LINK_PENDING, AUTH_PENDING, AUTH_SUCCESS, AUTH_EXPIRED
+}
