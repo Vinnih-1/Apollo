aefecef7f5e13811a8191bb1016a1b17586fea41
Vinnih-1 <vinifantal@hotmail.com>
2023-10-26 03:04:32 -0300

    add: listen authorize events

--- Discord/src/main/java/com/microservice/discord/consumers/AuthorizeServiceConsumer.java
+++ Discord/src/main/java/com/microservice/discord/consumers/AuthorizeServiceConsumer.java
@@ -1,19 +1,43 @@
 package com.microservice.discord.consumers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microservice.discord.consumers.events.AuthExpiredEvent;
+import com.microservice.discord.consumers.events.AuthPendingEvent;
+import com.microservice.discord.consumers.events.AuthSuccessEvent;
+import com.microservice.discord.consumers.events.BaseAuthEvent;
 import com.microservice.discord.models.AuthorizeModel;
+import com.microservice.discord.services.discord.DiscordService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Component
 public class AuthorizeServiceConsumer {
 
+    @Autowired
+    private DiscordService discordService;
+
+    private final List<BaseAuthEvent> events = Arrays.asList(
+            new AuthExpiredEvent(),
+            new AuthPendingEvent(),
+            new AuthSuccessEvent()
+    );
+
     @RabbitListener(queues =  "${broker.queue.auth.service}")
     public void listenAuthorizeServiceQueue(@Payload AuthorizeModel authorizeModel) {
-        System.out.println(authorizeModel.toString());
+        var guild = discordService.getJda().getGuildById(authorizeModel.getDiscordId().split("_")[0]);
+        var channel = guild.getTextChannelById(authorizeModel.getDiscordId().split("_")[1]);
+
+        events.stream()
+                .filter(event -> event.getAuthStatus() == authorizeModel.getAuthStatus())
+                .findFirst()
+                .ifPresent(event -> event.execute(guild, channel, authorizeModel));
     }
 
     @Bean
