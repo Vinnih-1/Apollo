4a76ea25d304018e9793f717dca3081184a339c5
Vinnih-1 <vinifantal@hotmail.com>
2023-11-02 04:17:08 -0300

    add: category id and products attributes

--- Service/src/main/java/com/apollo/microservice/service/dtos/ServiceDTO.java
+++ Service/src/main/java/com/apollo/microservice/service/dtos/ServiceDTO.java
@@ -4,9 +4,14 @@ import com.apollo.microservice.service.enums.ServiceType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.List;
+
 public record ServiceDTO(
         String serviceId,
         @NotNull @NotBlank String owner,
         String discordId,
-        @NotNull ServiceType serviceType) {
+        String categoryId,
+        @NotNull ServiceType serviceType,
+        List<ProductDTO> products
+        ) {
 }
