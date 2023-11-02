40039d3bb65e63f4af0ae0d72b7e2c0a1f848814
Vinnih-1 <vinifantal@hotmail.com>
2023-11-02 04:15:40 -0300

    add: generate payment endpoint

--- Service/src/main/java/com/apollo/microservice/service/controllers/ProductController.java
+++ Service/src/main/java/com/apollo/microservice/service/controllers/ProductController.java
@@ -1,7 +1,12 @@
 package com.apollo.microservice.service.controllers;
 
 import com.apollo.microservice.service.dtos.ProductDTO;
+import com.apollo.microservice.service.enums.PaymentIntent;
+import com.apollo.microservice.service.enums.PaymentStatus;
+import com.apollo.microservice.service.models.PaymentModel;
 import com.apollo.microservice.service.models.ProductModel;
+import com.apollo.microservice.service.producers.ServicePaymentProducer;
+import com.apollo.microservice.service.repositories.PaymentRepository;
 import com.apollo.microservice.service.repositories.ProductRepository;
 import com.apollo.microservice.service.repositories.ServiceRepository;
 import jakarta.validation.Valid;
@@ -16,10 +21,53 @@ import java.util.Calendar;
 public class ProductController {
 
     @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Autowired
     private ServiceRepository serviceRepository;
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private ServicePaymentProducer servicePaymentProducer;
+
+    @GetMapping("{id}/payment")
+    public ResponseEntity<PaymentModel> generateProductPayment(
+            @PathVariable(value = "id") Long id,
+            @RequestParam("payer") String payer,
+            @RequestParam("chatId") String chatId) {
+        var product = productRepository.findById(id).orElse(null);
+
+        if (product == null)
+            return ResponseEntity.badRequest().build();
+
+        var service = serviceRepository.findById(product.getServiceId()).orElse(null);
+
+        if (service == null)
+            return ResponseEntity.badRequest().build();
+
+        var expirateAt = Calendar.getInstance();
+        expirateAt.add(Calendar.MINUTE, 30);
+
+        var paymentModel = PaymentModel.builder()
+                .payer(payer)
+                .productId(id)
+                .paymentStatus(PaymentStatus.PENDING)
+                .paymentIntent(PaymentIntent.SELL_PRODUCT)
+                .createAt(Calendar.getInstance())
+                .expirateAt(expirateAt)
+                .serviceId(service.getId())
+                .chatId(chatId)
+                .accessToken(service.getAccessToken())
+                .coupon(null)
+                .build();
+
+        paymentRepository.saveAndFlush(paymentModel);
+        servicePaymentProducer.publishCreatePaymentMessage(paymentModel);
+
+        return ResponseEntity.ok(paymentModel);
+    }
 
     @PostMapping("/create")
     public ResponseEntity<ProductModel> createServiceProduct(@Valid @RequestBody ProductDTO productDTO) {
