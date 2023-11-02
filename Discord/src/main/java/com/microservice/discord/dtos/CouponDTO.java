a930753ab726aab2f0548edc34682797a62c6c8d
Vinnih-1 <vinifantal@hotmail.com>
2023-11-02 04:10:24 -0300

    add: coupon dto

---
+++ Discord/src/main/java/com/microservice/discord/dtos/CouponDTO.java
@@ -0,0 +1,14 @@
+package com.microservice.discord.dtos;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Embeddable
+public record CouponDTO(@Column(insertable = false, updatable = false) Long couponId,
+                        @NotNull @NotBlank String name,
+                        @NotNull Integer discount,
+                        @NotNull Integer expirateDays,
+                        @Column(insertable = false, updatable = false) String serviceId) {
+}
