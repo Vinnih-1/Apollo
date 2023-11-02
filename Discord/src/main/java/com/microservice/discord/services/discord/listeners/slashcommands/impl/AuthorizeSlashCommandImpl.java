6b833ef064129e742c5ea1ed10b92c8517d5871b
Vinnih-1 <vinifantal@hotmail.com>
2023-11-02 04:08:30 -0300

    fix: rename to AuthorizeSlashCommandImpl

--- Discord/src/main/java/com/microservice/discord/services/discord/listeners/slashcommands/impl/AuthorizeSlashCommandImpl.java
+++ Discord/src/main/java/com/microservice/discord/services/discord/listeners/slashcommands/impl/AuthorizeSlashCommandImpl.java
@@ -7,9 +7,9 @@ import net.dv8tion.jda.api.interactions.components.text.TextInput;
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 import net.dv8tion.jda.api.interactions.modals.Modal;
 
-public class AuthorizeSlashCommand extends BaseSlashCommand {
+public class AuthorizeSlashCommandImpl extends BaseSlashCommand {
 
-    public AuthorizeSlashCommand() {
+    public AuthorizeSlashCommandImpl() {
         super(
                 "slash.auth",
                 "autorizar",
