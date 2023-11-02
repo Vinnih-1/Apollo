467ddb5fe3100073679f586a040f23293705b931
Vinnih-1 <vinifantal@hotmail.com>
2023-11-02 04:06:29 -0300

    add: categoryId and chatId attributes

--- Discord/src/main/java/com/microservice/discord/dtos/AuthorizeDTO.java
+++ Discord/src/main/java/com/microservice/discord/dtos/AuthorizeDTO.java
@@ -4,6 +4,8 @@ public record AuthorizeDTO(
         String serviceId,
         String serviceKey,
         String discordId,
+        String categoryId,
+        String chatId,
         String authorizeUrl,
         String accessToken
 ) {
