9e8d52c0ca9a9e5e6edcb4a321b25d28576ccd9d
Vinnih-1 <vinifantal@hotmail.com>
2023-11-04 10:29:58 -0300

    add: apollo eureka server

---
+++ ApolloEureka/src/main/java/com/apollo/eureka/ApolloEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.apollo.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApolloEurekaApplication.class, args);
+	}
+
+}
---
+++ ApolloEureka/src/test/java/com/apollo/eureka/ApolloEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apollo.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApolloEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
