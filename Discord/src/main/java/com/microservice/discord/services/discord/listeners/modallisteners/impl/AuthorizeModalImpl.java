4e3452ce0bce1a84904472f98e312969526cbc18
Vinnih-1 <vinifantal@hotmail.com>
2023-11-30 23:18:23 -0300

    add: onFailure callback

--- Discord/src/main/java/com/microservice/discord/services/discord/listeners/modallisteners/impl/AuthorizeModalImpl.java
+++ Discord/src/main/java/com/microservice/discord/services/discord/listeners/modallisteners/impl/AuthorizeModalImpl.java
@@ -1,11 +1,14 @@
 package com.microservice.discord.services.discord.listeners.modallisteners.impl;
 
 import com.microservice.discord.dtos.AuthorizeDTO;
+import com.microservice.discord.messages.AuthorizeMessages;
 import com.microservice.discord.requests.ServiceRequest;
 import com.microservice.discord.services.discord.listeners.modallisteners.BaseModalListener;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 
+import java.awt.*;
 import java.util.List;
 
 public class AuthorizeModalImpl extends BaseModalListener {
@@ -42,7 +45,20 @@ public class AuthorizeModalImpl extends BaseModalListener {
 
                     ServiceRequest.getInstance()
                             .authorizeRequest(authorizeDto, onSuccess -> {
-                                event.getChannel().sendMessage(onSuccess.getAuthorizeUrl()).queue();
+                                event.getChannel().sendMessageEmbeds(
+                                        AuthorizeMessages.AUTHORIZATION_LINK(onSuccess.getAuthorizeUrl(),
+                                                event.getMember().getUser().getName())
+                                                .build()
+                                ).queue();
+                            }, onFailure -> {
+                                event.getChannel().sendMessageEmbeds(new EmbedBuilder()
+                                                .setColor(Color.RED)
+                                                .setDescription("Não foi possível vincular este serviço! " +
+                                                        "Verifique se os campos estão corretos.")
+                                        .build())
+                                        .queue();
+
+                                category.delete().queue();
                             });
                 });
     }
