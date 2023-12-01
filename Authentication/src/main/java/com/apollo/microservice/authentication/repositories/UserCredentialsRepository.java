873facc1f7b08eb465957a451b5ecc9a0ea21490
Vinnih-1 <vinifantal@hotmail.com>
2023-11-30 23:04:29 -0300

    add: exists by email method and find by email

--- Authentication/src/main/java/com/apollo/microservice/authentication/repositories/UserCredentialsRepository.java
+++ Authentication/src/main/java/com/apollo/microservice/authentication/repositories/UserCredentialsRepository.java
@@ -1,2 +1,13 @@
-package com.apollo.microservice.authentication.repositories;public interface UserCredentialsRepository {
+package com.apollo.microservice.authentication.repositories;
+
+import com.apollo.microservice.authentication.entities.UserCredentials;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserCredentialsRepository extends JpaRepository<UserCredentials, String> {
+
+    Optional<UserCredentials> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
