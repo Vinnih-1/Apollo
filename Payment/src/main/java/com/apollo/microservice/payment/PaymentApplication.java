03390f3138dc0075dea4371e826ab258c1aba746
Vinnih-1 <vinifantal@hotmail.com>
2023-11-30 23:03:51 -0300

    add: enable discovery client

--- Payment/src/main/java/com/apollo/microservice/payment/PaymentApplication.java
+++ Payment/src/main/java/com/apollo/microservice/payment/PaymentApplication.java
@@ -2,9 +2,11 @@ package com.apollo.microservice.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableScheduling
 public class PaymentApplication {
 
