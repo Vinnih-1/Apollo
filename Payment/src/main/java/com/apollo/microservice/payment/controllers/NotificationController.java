3fe4a495fcad3c01b7c85d4e81bde79b31c44068
Vinnih-1 <vinifantal@hotmail.com>
2023-11-30 23:12:12 -0300

    add: request mapping

--- Payment/src/main/java/com/apollo/microservice/payment/controllers/NotificationController.java
+++ Payment/src/main/java/com/apollo/microservice/payment/controllers/NotificationController.java
@@ -4,12 +4,10 @@ import com.apollo.microservice.payment.dtos.NotificationDTO;
 import com.apollo.microservice.payment.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/payment")
 public class NotificationController {
 
     @Autowired
