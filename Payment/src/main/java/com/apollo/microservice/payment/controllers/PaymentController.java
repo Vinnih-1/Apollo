ec135e40946a080f448860fd77d990b30412778a
Vinnih-1 <vinifantal@hotmail.com>
2023-11-30 23:11:40 -0300

    fix: slash request mapping

--- Payment/src/main/java/com/apollo/microservice/payment/controllers/PaymentController.java
+++ Payment/src/main/java/com/apollo/microservice/payment/controllers/PaymentController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Calendar;
 
 @RestController
-@RequestMapping("payment/service")
+@RequestMapping("/payment/service")
 public class PaymentController {
 
     @Autowired
