be5555cd8012c4156796fa80d70a3f80e7e95006
Vinnih-1 <vinifantal@hotmail.com>
2023-11-30 23:04:13 -0300

    add: add allowed origins

--- Authentication/src/main/java/com/apollo/microservice/authentication/configurations/WebMvcConfig.java
+++ Authentication/src/main/java/com/apollo/microservice/authentication/configurations/WebMvcConfig.java
@@ -1,2 +1,18 @@
-package com.apollo.microservice.authentication.configurations;public class WebMvcConfig {
+package com.apollo.microservice.authentication.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("**") //
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
 }
