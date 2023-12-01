dda0b5a09f678fa8d40e05f77a0a30abd0998ed1
Vinnih-1 <vinifantal@hotmail.com>
2023-11-30 23:07:49 -0300

    fix: queue names

--- Discord/src/main/java/com/microservice/discord/configs/RabbitMQConfig.java
+++ Discord/src/main/java/com/microservice/discord/configs/RabbitMQConfig.java
@@ -1,18 +1,14 @@
 package com.microservice.discord.configs;
 
 import org.springframework.amqp.core.Queue;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RabbitMQConfig {
 
-    @Value(value = "${broker.queue.discord.name}")
-    private String queue;
-
     @Bean
     public Queue queue() {
-        return new Queue(queue, true);
+        return new Queue("payment.discord", true);
     }
 }
--- Payment/src/main/java/com/apollo/microservice/payment/configs/RabbitMQConfig.java
+++ Payment/src/main/java/com/apollo/microservice/payment/configs/RabbitMQConfig.java
@@ -1,18 +1,14 @@
 package com.apollo.microservice.payment.configs;
 
 import org.springframework.amqp.core.Queue;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
-    @Value(value = "${broker.queue.payment.name}")
-    private String queue;
-
     @Bean
     public Queue queue() {
-        return new Queue(queue, true);
+        return new Queue("producer.payment", true);
     }
 }
