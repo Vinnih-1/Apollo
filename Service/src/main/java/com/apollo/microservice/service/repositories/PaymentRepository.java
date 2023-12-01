5d0dd3d7fe6866409d9c572559b7e7a155445f37
Vinnih-1 <vinifantal@hotmail.com>
2023-11-30 23:10:29 -0300

    add: declarative methods

--- Service/src/main/java/com/apollo/microservice/service/repositories/PaymentRepository.java
+++ Service/src/main/java/com/apollo/microservice/service/repositories/PaymentRepository.java
@@ -3,5 +3,11 @@ package com.apollo.microservice.service.repositories;
 import com.apollo.microservice.service.models.PaymentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<PaymentModel, String> {
+
+    Optional<PaymentModel> findByExternalReference(String externalReference);
+
+    void deleteByExternalReference(String externalReference);
 }
