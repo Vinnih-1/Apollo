433c19a40b468682ff356977eada3d6706df9a3f
Vinnih-1 <vinifantal@hotmail.com>
2023-11-30 23:11:51 -0300

    fix: queue name

--- Service/src/main/java/com/apollo/microservice/service/producers/PaymentAuthorizeProducer.java
+++ Service/src/main/java/com/apollo/microservice/service/producers/PaymentAuthorizeProducer.java
@@ -3,7 +3,6 @@ package com.apollo.microservice.service.producers;
 import com.apollo.microservice.service.models.AuthorizeModel;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,10 +11,7 @@ public class PaymentAuthorizeProducer {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    @Value(value = "${broker.queue.auth.service}")
-    private String discordRoutingKey;
-
     public void publishPaymentAuthorizeResponse(AuthorizeModel authorizeModel) {
-        rabbitTemplate.convertAndSend("", discordRoutingKey, authorizeModel);
+        rabbitTemplate.convertAndSend("", "authorizer.discord", authorizeModel);
     }
 }
