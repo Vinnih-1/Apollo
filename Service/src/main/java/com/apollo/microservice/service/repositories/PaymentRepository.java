0d7768eea8142fed48a96aea4ff8ca3dfa2ce47e
Vinnih-1 <vinifantal@hotmail.com>
2023-12-25 22:56:06 -0300

    add: add find all by service id method and filtered

--- Service/src/main/java/com/apollo/microservice/service/repositories/PaymentRepository.java
+++ Service/src/main/java/com/apollo/microservice/service/repositories/PaymentRepository.java
@@ -1,13 +1,21 @@
 package com.apollo.microservice.service.repositories;
 
+import com.apollo.microservice.service.enums.PaymentStatus;
 import com.apollo.microservice.service.models.PaymentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<PaymentModel, String> {
 
     Optional<PaymentModel> findByExternalReference(String externalReference);
+
+    Optional<List<PaymentModel>> findAllByServiceId(String serviceId);
+
+    @Query("SELECT p FROM PaymentModel p WHERE p.serviceId = :serviceId AND p.paymentStatus = :paymentStatus")
+    Optional<List<PaymentModel>> findAllFilteredByServiceId(String serviceId, PaymentStatus paymentStatus);
 
     void deleteByExternalReference(String externalReference);
 }
