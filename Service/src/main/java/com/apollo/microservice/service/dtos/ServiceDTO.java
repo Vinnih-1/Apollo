b10c433b063e628e3857cbdd0ba4740ecace4f3c
Vinnih-1 <vinifantal@hotmail.com>
2023-12-25 23:00:06 -0300

    fix: from record to class

--- Service/src/main/java/com/apollo/microservice/service/dtos/ServiceDTO.java
+++ Service/src/main/java/com/apollo/microservice/service/dtos/ServiceDTO.java
@@ -1,17 +1,36 @@
 package com.apollo.microservice.service.dtos;
 
 import com.apollo.microservice.service.enums.ServiceType;
+import com.apollo.microservice.service.models.ProductModel;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
-public record ServiceDTO(
-        String serviceId,
-        @NotNull @NotBlank String owner,
-        String discordId,
-        String categoryId,
-        @NotNull ServiceType serviceType,
-        List<ProductDTO> products
-        ) {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServiceDTO {
+
+        private String id;
+
+        private String serviceKey;
+
+        @NotNull @NotBlank private String owner;
+
+        private String discordId;
+
+        private String categoryId;
+
+        @NotNull private  ServiceType serviceType;
+
+        private long buyers;
+
+        private double moneyMoved;
+
+        private List<ProductDTO> products;
 }
