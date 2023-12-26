4b6a756dc66a63de28ee6141a032c98fb4a505f8
Vinnih-1 <vinifantal@hotmail.com>
2023-12-25 22:52:52 -0300

    add: find coupons by service id method

--- Service/src/main/java/com/apollo/microservice/service/repositories/CouponRepository.java
+++ Service/src/main/java/com/apollo/microservice/service/repositories/CouponRepository.java
@@ -3,5 +3,10 @@ package com.apollo.microservice.service.repositories;
 import com.apollo.microservice.service.models.CouponModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CouponRepository extends JpaRepository<CouponModel, Long> {
+
+    Optional<List<CouponModel>> findCouponsByServiceId(String serviceId);
 }
