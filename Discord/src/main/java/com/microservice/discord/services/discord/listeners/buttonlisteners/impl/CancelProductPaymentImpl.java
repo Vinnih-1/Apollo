02f302fdfd874249253c047d3e6de4df0ee8e678
Vinnih-1 <vinifantal@hotmail.com>
2023-12-31 15:09:28 -0300

    fix: add @Component annotation and mark with prototype scope

--- Discord/src/main/java/com/microservice/discord/services/discord/listeners/buttonlisteners/impl/CancelProductPaymentImpl.java
+++ Discord/src/main/java/com/microservice/discord/services/discord/listeners/buttonlisteners/impl/CancelProductPaymentImpl.java
@@ -2,7 +2,11 @@ package com.microservice.discord.services.discord.listeners.buttonlisteners.impl
 
 import com.microservice.discord.services.discord.listeners.buttonlisteners.BaseButtonListener;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
+@Component
+@Scope("prototype")
 public class CancelProductPaymentImpl extends BaseButtonListener {
 
     public CancelProductPaymentImpl() {
