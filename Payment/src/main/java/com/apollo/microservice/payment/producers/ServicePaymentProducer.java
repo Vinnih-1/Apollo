565b942468769c8e51732c3bf62f8ee313ebcb2b
Vinnih-1 <vinifantal@hotmail.com>
2023-12-31 14:57:46 -0300

    fix: publish payment DTO instead payment model

--- Payment/src/main/java/com/apollo/microservice/payment/producers/ServicePaymentProducer.java
+++ Payment/src/main/java/com/apollo/microservice/payment/producers/ServicePaymentProducer.java
@@ -1,6 +1,6 @@
 package com.apollo.microservice.payment.producers;
 
-import com.apollo.microservice.payment.models.PaymentModel;
+import com.apollo.microservice.payment.dtos.PaymentDTO;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,7 +11,7 @@ public class ServicePaymentProducer {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    public void sendProductPaymentMessage(PaymentModel paymentModel) {
-        rabbitTemplate.convertAndSend("payment", "", paymentModel);
+    public void sendProductPaymentMessage(PaymentDTO paymentDTO) {
+        rabbitTemplate.convertAndSend("payment", "", paymentDTO);
     }
 }
