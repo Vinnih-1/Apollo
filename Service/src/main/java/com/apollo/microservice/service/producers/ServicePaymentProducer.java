152b9db2eb19e3ca9622f78c7a2f4765cd04a00e
Vinnih-1 <vinifantal@hotmail.com>
2023-12-31 14:57:29 -0300

    fix: publish payment modal instead his DTO

--- Service/src/main/java/com/apollo/microservice/service/producers/ServicePaymentProducer.java
+++ Service/src/main/java/com/apollo/microservice/service/producers/ServicePaymentProducer.java
@@ -1,9 +1,6 @@
 package com.apollo.microservice.service.producers;
 
-import com.apollo.microservice.service.dtos.PaymentDTO;
 import com.apollo.microservice.service.models.PaymentModel;
-import com.apollo.microservice.service.repositories.CouponRepository;
-import com.apollo.microservice.service.repositories.ProductRepository;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,30 +11,14 @@ public class ServicePaymentProducer {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    @Autowired
-    private CouponRepository couponRepository;
-
-    @Autowired
-    private ProductRepository productRepository;
-
+    /**
+     * Publica o objeto de pagamento pronto para ser utilizado
+     * pelo microsserviço Discord, já contendo os dados
+     * do QRCode.
+     *
+     * @param paymentModel
+     */
     public void publishCreatePaymentMessage(PaymentModel paymentModel) {
-        var product = productRepository.findById(paymentModel.getProductId()).orElse(null);
-
-        if (product == null) return;
-
-        var paymentDTO = new PaymentDTO(
-                paymentModel.getId(),
-                paymentModel.getPayer(),
-                product.getService().getId(),
-                paymentModel.getChatId(),
-                paymentModel.getAccessToken(),
-                paymentModel.getCoupon() != null ? paymentModel.getCoupon() : null,
-                paymentModel.getPaymentStatus(),
-                paymentModel.getPaymentIntent(),
-                product.getPrice(),
-                paymentModel.getProductId()
-        );
-
-        rabbitTemplate.convertAndSend("", "producer.payment", paymentDTO);
+        rabbitTemplate.convertAndSend("payment", "", paymentModel);
     }
 }
