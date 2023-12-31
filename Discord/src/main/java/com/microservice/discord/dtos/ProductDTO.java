689f9872aa52ee1d50d8044fae44938dba057832
Vinnih-1 <vinifantal@hotmail.com>
2023-12-31 15:01:20 -0300

    add: Product Data Transfer Object

--- Discord/src/main/java/com/microservice/discord/dtos/ProductDTO.java
+++ Discord/src/main/java/com/microservice/discord/dtos/ProductDTO.java
@@ -1,10 +1,34 @@
 package com.microservice.discord.dtos;
 
-public record ProductDTO(
-        Long id,
-        String name,
-        String description,
-        double price,
-        String serviceId
-) {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ProductDTO {
+
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    private String name;
+
+    private String description;
+
+    @NotNull
+    private double price;
+
+    private String serviceId;
+
+    private List<PaymentDTO> payments;
 }
--- Payment/src/main/java/com/apollo/microservice/payment/dtos/ProductDTO.java
+++ Payment/src/main/java/com/apollo/microservice/payment/dtos/ProductDTO.java
@@ -1,2 +1,32 @@
-package com.apollo.microservice.payment.dtos;public class ProductDTO {
+package com.apollo.microservice.payment.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Calendar;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDTO {
+
+    private Long id;
+
+    private String name;
+
+    private double price;
+
+    private List<PaymentDTO> payments;
+
+    private ServiceDTO service;
+
+    private String serviceId;
+
+    private String description;
+
+    private Calendar createAt;
 }
