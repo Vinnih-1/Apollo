ef70b8d55e64dcc4f4f6531c84e4f1f65704ba56
Vinnih-1 <vinifantal@hotmail.com>
2023-12-31 15:10:39 -0300

    fix: Service DTO instead AuthorizeModel

--- Discord/src/main/java/com/microservice/discord/consumers/events/AuthSuccessEvent.java
+++ Discord/src/main/java/com/microservice/discord/consumers/events/AuthSuccessEvent.java
@@ -1,9 +1,8 @@
 package com.microservice.discord.consumers.events;
 
-import com.microservice.discord.consumers.events.BaseAuthEvent;
+import com.microservice.discord.dtos.ServiceDTO;
 import com.microservice.discord.enums.AuthStatus;
 import com.microservice.discord.messages.AuthorizeMessages;
-import com.microservice.discord.models.AuthorizeModel;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 
@@ -13,10 +12,18 @@ public class AuthSuccessEvent extends BaseAuthEvent {
         super(AuthStatus.AUTH_SUCCESS);
     }
 
+    /**
+     * Envia a mensagem de autorização realizada com êxito
+     * no chat em que o comando /autorizar foi executado.
+     *
+     * @param guild
+     * @param textChannel
+     * @param serviceDTO
+     */
     @Override
-    public void execute(Guild guild, TextChannel textChannel, AuthorizeModel authorizeModel) {
+    public void execute(Guild guild, TextChannel textChannel, ServiceDTO serviceDTO) {
         textChannel.sendMessageEmbeds(
-                AuthorizeMessages.AUTHORIZATION_SUCCESSFUL(authorizeModel.getServiceId()).build()
+                AuthorizeMessages.AUTHORIZATION_SUCCESSFUL(serviceDTO.getId()).build()
         ).queue();
     }
 }
