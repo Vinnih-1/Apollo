a109a62d8cb81773baf2c37f77eb52202f26a44c
Vinnih-1 <vinifantal@hotmail.com>
2023-12-31 15:07:54 -0300

    remove: delete unused method

--- Service/src/main/java/com/apollo/microservice/service/repositories/CouponRepository.java
+++ Service/src/main/java/com/apollo/microservice/service/repositories/CouponRepository.java
@@ -3,10 +3,5 @@ package com.apollo.microservice.service.repositories;
 import com.apollo.microservice.service.models.CouponModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface CouponRepository extends JpaRepository<CouponModel, Long> {
-
-    Optional<List<CouponModel>> findCouponsByServiceId(String serviceId);
 }
