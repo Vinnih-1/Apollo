80e59aa5b23880a639bbace490da0fbdd95ed3db
Vinnih-1 <vinifantal@hotmail.com>
2023-12-31 15:10:26 -0300

    fix: Service DTO instead AuthorizeModel

--- Discord/src/main/java/com/microservice/discord/consumers/events/BaseAuthEvent.java
+++ Discord/src/main/java/com/microservice/discord/consumers/events/BaseAuthEvent.java
@@ -1,8 +1,7 @@
 package com.microservice.discord.consumers.events;
 
+import com.microservice.discord.dtos.ServiceDTO;
 import com.microservice.discord.enums.AuthStatus;
-import com.microservice.discord.models.AuthorizeModel;
-import com.microservice.discord.services.discord.DiscordService;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import net.dv8tion.jda.api.entities.Guild;
@@ -14,5 +13,5 @@ public abstract class BaseAuthEvent {
 
     private AuthStatus authStatus;
 
-    public abstract void execute(Guild guild, TextChannel textChannel, AuthorizeModel authorizeModel);
+    public abstract void execute(Guild guild, TextChannel textChannel, ServiceDTO serviceDTO);
 }
