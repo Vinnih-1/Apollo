ca6416e7aff6f1824f89048e22d0c88ec5ab0505
Vinnih-1 <vinifantal@hotmail.com>
2023-12-31 14:53:39 -0300

    add: enable feign clients

--- Discord/src/main/java/com/microservice/discord/DiscordApplication.java
+++ Discord/src/main/java/com/microservice/discord/DiscordApplication.java
@@ -3,13 +3,15 @@ package com.microservice.discord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class DiscordApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscordApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordApplication.class, args);
+    }
 
 }
