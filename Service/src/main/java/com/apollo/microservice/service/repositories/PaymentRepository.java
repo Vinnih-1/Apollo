579fc86fc470c289f4a0af19e5fa48e4035011b9
Vinnih-1 <vinifantal@hotmail.com>
2024-01-03 16:59:53 -0300

    add: custom query to select with pages

--- Service/src/main/java/com/apollo/microservice/service/repositories/PaymentRepository.java
+++ Service/src/main/java/com/apollo/microservice/service/repositories/PaymentRepository.java
@@ -1,13 +1,19 @@
 package com.apollo.microservice.service.repositories;
 
 import com.apollo.microservice.service.models.PaymentModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<PaymentModel, String> {
 
     Optional<PaymentModel> findByExternalReference(String externalReference);
+
+    @Query("SELECT p FROM PaymentModel p WHERE p.product.service.id = :serviceId")
+    Page<PaymentModel> findAllByServiceId(Pageable pageable, String serviceId);
 
     void deleteByExternalReference(String externalReference);
 }
