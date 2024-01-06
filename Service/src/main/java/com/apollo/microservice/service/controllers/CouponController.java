576de9bca734d408c3cda1a267ef4b060a560fcd
Vinnih-1 <vinifantal@hotmail.com>
2024-01-05 21:15:29 -0300

    add: coupon service dependency

--- Service/src/main/java/com/apollo/microservice/service/controllers/CouponController.java
+++ Service/src/main/java/com/apollo/microservice/service/controllers/CouponController.java
@@ -3,6 +3,7 @@ package com.apollo.microservice.service.controllers;
 import com.apollo.microservice.service.clients.UserClient;
 import com.apollo.microservice.service.dtos.CouponDTO;
 import com.apollo.microservice.service.models.CouponModel;
+import com.apollo.microservice.service.services.CouponService;
 import com.apollo.microservice.service.services.PlanService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,9 @@ public class CouponController {
 
     @Autowired
     private PlanService planService;
+
+    @Autowired
+    private CouponService couponService;
 
     @Autowired
     private UserClient userClient;
@@ -39,21 +43,21 @@ public class CouponController {
         if (service == null) {
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(planService.createNewCoupon(couponDTO, service));
+        return ResponseEntity.ok(couponService.createNewCoupon(couponDTO, service));
     }
 
     @DeleteMapping("/delete")
     public ResponseEntity<Void> deleteCoupon(@RequestParam("id") long id, @RequestHeader("Authorization") String token) {
         var user = userClient.userByToken(token);
         var service = planService.findByOwner(user.getEmail());
-        var coupon = planService.findByCouponId(id);
+        var coupon = couponService.findByCouponId(id);
         if (coupon == null) {
             return ResponseEntity.notFound().build();
         }
         if (!coupon.getService().getId().equals(service.getId())) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
-        planService.deleteCouponById(id);
+        couponService.deleteCouponById(id);
         return ResponseEntity.ok().build();
     }
 }
