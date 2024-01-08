2741ebe804c3d92e0e4a61fd78f9809dc4d484a6
Vinnih-1 <vinifantal@hotmail.com>
2024-01-08 03:37:03 -0300

    add: userDTO

---
+++ Service/src/main/java/com/apollo/microservice/service/dtos/UserDTO.java
@@ -0,0 +1,15 @@
+package com.apollo.microservice.service.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserDTO {
+
+    private String email;
+}
